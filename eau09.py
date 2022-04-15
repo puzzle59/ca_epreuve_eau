9bc6ea7f356ff5149df319e4926fa3ef089e4855
Marvin <cimettamarvin@gmail.com>
2022-04-15 21:49:03 +0200

    commit du jour pas concluant

---
+++ eau09.py
@@ -0,0 +1,14 @@
+#Entre min et max
+import sys
+n1=int(sys.argv[1])
+n2=int(sys.argv[2])
+# def min_max(a,b):
+#     tableau=[]
+#     for i in range(a,b):
+#         tableau.append(i)
+#     print(tableau)
+#     for item in tableau:
+#         print(item)
+# print(min_max(n1,n2))
+# J'ignore pourquoi mais ça m'affiche None quand je print le tableau..
+autre idée; directement afficher via range 
