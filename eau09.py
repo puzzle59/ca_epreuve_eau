ac8be468a6edd9c8cd533e3d755100d05d66f55b
Marvin <cimettamarvin@gmail.com>
2022-04-16 17:01:24 +0200

    trouvé solution

--- eau09.py
+++ eau09.py
@@ -2,13 +2,13 @@
 import sys
 n1=int(sys.argv[1])
 n2=int(sys.argv[2])
-# def min_max(a,b):
-#     tableau=[]
-#     for i in range(a,b):
-#         tableau.append(i)
-#     print(tableau)
-#     for item in tableau:
-#         print(item)
-# print(min_max(n1,n2))
+def min_max(a,b):
+    tableau=[]
+    for i in range(a,b):
+        tableau.append(i)
+    for item in tableau:
+        print(item,end=" ")
+min_max(n1,n2)
 # J'ignore pourquoi mais ça m'affiche None quand je print le tableau..
-autre idée; directement afficher via range 
+#car, je n'ai pas mis return dans la fonction
+# autre idée; directement afficher via range
